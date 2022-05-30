78cc26b8eefa8cdd95091a9e3c2bd6faa3576df8
dkreeft <37153972+dkreeft@users.noreply.github.com>
2022-05-30 18:44:09 +0200

    add code and icons

---
+++ background.js
@@ -0,0 +1,13 @@
+const desiredLocale = "/en-us/";
+const regex = /\/[a-zA-z]{2}-[a-zA-Z]{2}\//i;
+
+chrome.webNavigation.onBeforeNavigate.addListener(function (address) {
+    if (address.url.indexOf("docs.microsoft.com") != -1) {
+        if (address.url.indexOf(desiredLocale) == -1) {
+            let newUrl = address.url.replace(regex, desiredLocale);
+            chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+                chrome.tabs.update(tab[0].id, {url: newUrl});
+            });
+        };
+    };
+});

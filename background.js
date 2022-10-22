af7d6a001e16012c6dfb5fc13bc8c4bdddd2d01d
dkreeft <37153972+dkreeft@users.noreply.github.com>
2022-10-22 11:27:41 +0200

    fix extension to work with learn.microsoft.com as microsoft has changed the domain of their docs

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ const desiredLocale = "/en-us/";
 const regex = /\/[a-zA-z]{2}-[a-zA-Z]{2}\//i;
 
 chrome.webNavigation.onBeforeNavigate.addListener(function (address) {
-    if (address.url.indexOf("docs.microsoft.com") != -1) {
+    if (address.url.indexOf("learn.microsoft.com") != -1) {
         if (address.url.indexOf(desiredLocale) == -1) {
             let newUrl = address.url.replace(regex, desiredLocale);
             chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
